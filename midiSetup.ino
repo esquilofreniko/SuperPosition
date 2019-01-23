b53b40a1eb9f9930a599c57a9c60e8e65ac1af6b
esquilofreniko <esquilofreniko@gmail.com>
2019-01-23 23:00:01 +0000

    better oled

--- midiSetup.ino
+++ midiSetup.ino
@@ -1,5 +1,9 @@
 void midiSetup(){
   if(oled_draw_bg == 1){
-    u8x8.drawString(0,0,String("midiIn: " + dectohex(midiIn_chan)).c_str());
+    for(int i=0;i<4;i++){
+      u8x8.drawString(0,i,String("MIDIOut" + String(i+1) + ": " + dectohex(midiOut_chan[i])).c_str());
+    }
+    u8x8.drawString(0,4,String("MIDI_In1: " + dectohex(midiIn_chan)).c_str());
+
   }
 }
