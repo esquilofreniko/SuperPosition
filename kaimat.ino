33d88e1101408aabbc3b2e9ab38b7b3d4e0ff634
esquilofreniko <esquilofreniko@gmail.com>
2019-02-25 01:42:37 +0000

    fixed oled

--- kaimat.ino
+++ kaimat.ino
@@ -14,12 +14,11 @@ void loop(){
   if(mode == 0){mainMenu.run();}
   if(mode == 1){midiSetup();}
   if(mode == 2){probSeq.run();}
-  if(oldmode != mode){oled.clear();oldmode=mode;}
-  if(oled.redraw == 1){oled.redraw = 0;}
+  if(oled.redraw == 1){oled.show();oled.redraw = 0;}
   if(millis() - oled.lastRedrawMS > 25){
-    oled.show();
     oled.clear();
     oled.redraw = 1;
     oled.lastRedrawMS = millis();
   }
+  if(oldmode != mode){oled.clear();oled.redraw=1;oldmode=mode;}
 }
